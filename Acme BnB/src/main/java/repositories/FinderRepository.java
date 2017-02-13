0b28c656769dcec87c6dfdd6e5de751ee8f77e4b
anapareciendo <ana.apariciora@gmail.com>
2017-02-13 17:15:22 +0100

    Método findMyFinders

--- Acme BnB/src/main/java/repositories/FinderRepository.java
+++ Acme BnB/src/main/java/repositories/FinderRepository.java
@@ -1,7 +1,10 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Finder;
@@ -9,4 +12,6 @@ import domain.Finder;
 @Repository
 public interface FinderRepository extends JpaRepository<Finder, Integer> {
 
+	@Query("select f from Finder f where f.tenant.userAccount.id = ?1")
+	Collection<Finder> findMyFinders(int id);
 }
