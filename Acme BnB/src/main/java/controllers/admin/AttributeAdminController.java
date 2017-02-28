bc833e1fc84537a36264b9d17ac63b8686476e75
Carmen <Student@10.0.2.15>
2017-02-28 17:03:33 +0100

    security

--- Acme BnB/src/main/java/controllers/admin/AttributeAdminController.java
+++ Acme BnB/src/main/java/controllers/admin/AttributeAdminController.java
@@ -12,8 +12,11 @@ package controllers.admin;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,6 +69,56 @@ public class AttributeAdminController extends AbstractController {
 		}catch(Throwable oops){
 			result= new ModelAndView("hacker/hackers");
 		}
+		return result;
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView result;
+		Attribute attribute= attributeService.create();
+		result = new ModelAndView("attribute/edit");
+		result.addObject("attribute", attribute);
+		
+		return result;
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid Attribute attribute, BindingResult binding) {
+		ModelAndView result;
+
+		if (binding.hasErrors()) {
+			result = createEditModelAndView(attribute);
+			result = new ModelAndView("attribute/edit");
+			result.addObject("attribute",attribute);
+			result.addObject("errors", binding.getAllErrors());
+		} else {
+			try {
+				attributeService.save(attribute);				
+				result = new ModelAndView("redirect:list.do");
+			} catch (Throwable oops) {
+				result = createEditModelAndView(attribute, "attribute.commit.error");				
+			}
+		}
+
+		return result;
+	}
+	
+	
+	protected ModelAndView createEditModelAndView(Attribute attribute) {
+		ModelAndView result;
+
+		result = createEditModelAndView(attribute, null);
+		
+		return result;
+	}	
+	
+	protected ModelAndView createEditModelAndView(Attribute attribute, String message) {
+		ModelAndView result;
+		
+		result = new ModelAndView("attribute/list");
+		result.addObject("attribute", attribute);
+		result.addObject("message", message);
+
 		return result;
 	}
 }
